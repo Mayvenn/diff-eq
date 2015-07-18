645bfbc951ada5548ce069ff82b681f34eff94f9
Jeff Hui <jeff@mayvenn.com>
2015-07-17 18:14:41 -0700

    Bump to 0.1.1 snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject diff-eq "0.1.0"
+(defproject diff-eq "0.1.1-SNAPSHOT"
   :description "Equality diff for humans"
   :url "http://github.com/mayvenn/diff-eq"
   :license {:name "Eclipse Public License"
