f73a2cb9f802bae0f4c8e0080ee310a81bd35496
Jeff Hui <jeff@mayvenn.com>
2015-08-16 21:33:35 -0700

    Set version to 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject diff-eq "0.1.1-SNAPSHOT"
+(defproject diff-eq "0.2.0-SNAPSHOT"
   :description "Equality diff for humans"
   :url "http://github.com/mayvenn/diff-eq"
   :license {:name "Eclipse Public License"
