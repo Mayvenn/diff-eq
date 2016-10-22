1b28f283bd284887cfaa23ff8e265f6d829c747a
Jeff Hui <jeff@mayvenn.com>
2016-10-21 17:41:45 -0700

    Version 0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject diff-eq "0.2.4-SNAPSHOT"
+(defproject diff-eq "0.2.4"
   :description "Equality diff for humans"
   :url "http://github.com/mayvenn/diff-eq"
   :license {:name "Eclipse Public License"
